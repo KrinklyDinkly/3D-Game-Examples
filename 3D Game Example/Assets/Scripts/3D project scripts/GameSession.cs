0dea038ac13bd866bc965957852340b9461938ec
KrinklyDinkly <isaiah.rodriguez0207@gmail.com>
2024-02-27 12:55:28 -0800

    added more items

---
+++ 3D Game Example/Assets/Scripts/3D project scripts/GameSession.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] private int _CoinCount;
+    [SerializeField] private TextMeshProUGUI _countCountText;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
