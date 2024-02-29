0723ee697f99afa57f11858e0417b93c1f6d16cc
KrinklyDinkly <isaiah.rodriguez0207@gmail.com>
2024-02-29 12:53:59 -0800

    add fade out

--- 3D Game Example/Assets/Scripts/3D project scripts/GameSession.cs
+++ 3D Game Example/Assets/Scripts/3D project scripts/GameSession.cs
@@ -10,15 +10,57 @@ public class GameSession : MonoBehaviour
     [SerializeField] private int _CoinCount;
     [SerializeField] private TextMeshProUGUI _countCountText;
     [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private bool _fadeIn, _fadeOut;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _CoinCount = 0;
+        //_countCountText.text = "Coins:" + _CoinCount.ToString();
+        FadeOutUi();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_fadeIn)
+        {
+            if(_canvasGroup.alpha < 1)
+            {
+                _canvasGroup.alpha += Time.deltaTime;
+                if(_canvasGroup.alpha >= 1)
+                {
+                    _fadeIn = false;
+                }
+            }
+        }
+        if(_fadeOut)
+        {
+            if(_canvasGroup.alpha >= 0)
+            {
+                _canvasGroup.alpha -= Time.deltaTime;
+                if(_canvasGroup.alpha == 0)
+                {
+                    _fadeOut = false;
+                }
+            }
+        }
+    }
+
+    public void UpdateCoinCount(int amount)
+    {
+        _CoinCount += amount;
+        _countCountText.text = "Coins: " + _CoinCount.ToString();
+    }
+
+    public void FadeInUi()
+    {
+        //_canvasGroup.alpha = 0;
+        _fadeIn = true;
+    }
+
+    public void FadeOutUi()
+    {
+        //_canvasGroup.alpha = 1;
+        _fadeOut = true;
     }
 }
